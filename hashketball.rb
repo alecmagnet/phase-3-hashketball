1dccd96122bca0f32745ba5e12710829ba7991d3
Alec Magnet <amagnet@gmail.com>
2021-08-10 11:45:50 -0400

    completed deliverables

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 # Write your code below game_hash
 def game_hash
   {
@@ -127,3 +129,98 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(baller)
+  points = 0
+  game_hash[:home][:players].filter do |player| 
+    if player[:player_name] == baller
+      points = player[:points]
+    end
+  end
+  game_hash[:away][:players].filter do |player| 
+    if player[:player_name] == baller
+      points = player[:points]
+    end
+  end
+  points
+end
+
+def shoe_size(baller)
+  shoe = 0
+  game_hash[:home][:players].filter do |player| 
+    if player[:player_name] == baller
+      shoe = player[:shoe]
+    end
+  end
+  game_hash[:away][:players].filter do |player| 
+    if player[:player_name] == baller
+      shoe = player[:shoe]
+    end
+  end
+  shoe
+end
+
+def team_colors(team)
+  color=[]
+  if game_hash[:home][:team_name] == team
+    color = game_hash[:home][:colors]
+  else color = game_hash[:away][:colors]
+  end
+  color
+end
+
+def team_names  
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]]
+end
+
+def player_numbers(team)
+  jerseys = []
+  if game_hash[:home][:team_name] == team
+    jerseys = game_hash[:home][:players].map do |player|
+      player[:number]
+    end
+  else jerseys = game_hash[:away][:players].map do |player|
+      player[:number]
+  end
+  end
+  jerseys
+end
+
+def player_stats(baller)
+  stats = {}
+  game_hash[:home][:players].filter do |player|
+    if player[:player_name] == baller
+      stats = player
+    end
+  end
+  game_hash[:away][:players].filter do |player|
+    if player[:player_name] == baller
+      stats = player
+    end
+  end
+  stats
+end
+
+def big_shoe_rebounds
+  shoe_array = []
+  shoe_array_one = []
+  shoe_array_two = []
+  rebounds = 0
+  shoe_array_one = game_hash[:home][:players].map do |player|
+      player[:shoe]
+    end
+  shoe_array_two = game_hash[:away][:players].map do |player|
+      player[:shoe]
+    end
+  shoe_array = shoe_array_one + shoe_array_two
+  game_hash[:home][:players].find do |player|
+    if player[:shoe] == shoe_array.sort[-1]
+      rebounds = player[:rebounds]
+    end
+  end
+  game_hash[:away][:players].find do |player|
+    if player[:shoe] == shoe_array.sort[-1]
+      rebounds = player[:rebounds]
+    end
+  end
+  rebounds
+end
